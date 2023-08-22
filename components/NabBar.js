dbebfa6c968426edd1ab0ab21612e17cb5d3234c
khurshedeth <khurshed.eth@gmail.com>
2023-08-23 00:47:24 +0530

    Initial commit

---
+++ components/NabBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NabBar() {
+  return (
+    <div className=' text-red-600 flex flex-row justify-between'>
+    <div>image icon</div>
+    <div>
+    <a>1</a>
+    <a>2</a>
+    <a>3</a>
+    <a>4</a>
+    <a>5</a>
+
+    </div>
+    </div>
+  )
+}
+
+export default NabBar
