f54fb9f7495aa8591033c9ab62cf7189455a38e7
khurshedeth <khurshed.eth@gmail.com>
2023-09-08 02:40:10 +0530

    four projects are ther

--- components/Projects.js
+++ components/Projects.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
   return (
     <div>
-    
-    
+      <div>porfolio</div>
+      <div>Epployee management</div>
+      <div>Weather </div>
+      <div>transmit</div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
