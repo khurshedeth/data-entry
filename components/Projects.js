02f83c5cfad5aec166919a48af822b0cab9eb5ff
khurshedeth <khurshed.eth@gmail.com>
2023-09-09 02:23:44 +0530

    minor changes in projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Projects() {
   return (
-    <div className=" pt-28">
-      <div>portfolio</div>
+    <div
+      className=" flex flex-col justify-center mx-[1rem] pt-[12rem] lg:mx-[4rem]"
+      id="Projects"
+    >
+      <motion.h1
+        initial={{}}
+        whileInView={{}}
+        transition={{}}
+        viewport={{}}
+        className=" text-[3rem] font-bold mb-[2rem] text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-orange-700"
+      >
+        Projects
+      </motion.h1>
       <div>Epployee management</div>
       <div>Weather </div>
       <div>transmit</div>
