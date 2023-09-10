30aa9c6935210cc3839d71112ddb0472b32968ea
khurshedeth <khurshed.eth@gmail.com>
2023-09-11 02:26:03 +0530

    Projects card

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Project from "./Project";
 
 function Projects() {
   return (
@@ -8,17 +9,28 @@ function Projects() {
       id="Projects"
     >
       <motion.h1
-        initial={{}}
-        whileInView={{}}
-        transition={{}}
-        viewport={{}}
+        initial={{ opacity: 0 }}
+        whileInView={{ y: [-50, 0], opacity: 1 }}
+        transition={{ duration: 1 }}
         className=" text-[3rem] font-bold mb-[2rem] text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-orange-700"
       >
         Projects
       </motion.h1>
-      <div>Epployee management</div>
-      <div>Weather </div>
-      <div>transmit</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ x: [-100, 0], opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        className=" grid grid-cols-1 gap-[5rem] md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3"
+      >
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        
+      </motion.div>
     </div>
   );
 }
